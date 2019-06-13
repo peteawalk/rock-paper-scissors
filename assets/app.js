3e9b9576b1867296590d6f1a807b297294749fc4
Peter Walker <25408515+peteawalk@users.noreply.github.com>
2019-06-12 20:46:38 -0500

    initial commit - still in the works

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+// The underscore for DOM variables. HTML variables that store DOM elements.
+
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+// HTML id values are just in quotes.
+// HTML classes have a . (period) in the quotes.
